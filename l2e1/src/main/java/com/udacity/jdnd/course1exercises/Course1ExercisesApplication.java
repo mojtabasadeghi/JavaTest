f3f13d53f32ea2d87691ab5faecabebef549e5e8
mojtabasadeghi <47910283+mojtabasadeghi@users.noreply.github.com>
2020-12-03 11:43:00 -0800

    Add files via upload

---
+++ l2e1/src/main/java/com/udacity/jdnd/course1exercises/Course1ExercisesApplication.java
@@ -0,0 +1,15 @@
+package com.udacity.jdnd.course1exercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootConfiguration
+public class Course1ExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course1ExercisesApplication.class, args);
+	}
+
+
+}
